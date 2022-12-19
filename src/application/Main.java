1945dbb437d3ace1779babf97a36cf684f75925a
Jannisaurus <117642071+Jannisaurus@users.noreply.github.com>
2022-12-19 22:13:09 +0100

    The game

---
+++ src/application/Main.java
@@ -0,0 +1,31 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Play.fxml"));
+			Scene scene = new Scene(root,Color.DARKGOLDENROD);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Image somemaths = new Image("somemaths.jpg");
+			primaryStage.getIcons().add(somemaths); 
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
